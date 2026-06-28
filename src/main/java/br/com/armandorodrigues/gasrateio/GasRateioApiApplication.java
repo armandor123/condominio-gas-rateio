68e4dc2d66662fe8bec7b82d64440027b6733100
Armando Almeida <armando.r123@hotmail.com>
2026-06-28 19:43:09 -0300

    chore: configura projeto Spring Boot inicial

---
+++ src/main/java/br/com/armandorodrigues/gasrateio/GasRateioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.armandorodrigues.gasrateio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasRateioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasRateioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/armandorodrigues/gasrateio/GasRateioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.armandorodrigues.gasrateio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasRateioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
